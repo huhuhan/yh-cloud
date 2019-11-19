71f74529ea182344ac0617656aee7b747e646293
huhuhan <869108683@qq.com>
2019-11-19 16:40:37 +0800

    新增yh-common-base

---
+++ yh-common/yh-common-base/src/main/java/com/yh/cloud/base/constant/BaseConstant.java
@@ -0,0 +1,12 @@
+package com.yh.cloud.base.constant;
+
+/**
+ * 系统全局常量类
+ * @author yanghan
+ * @date 2019/7/29
+ */
+public interface BaseConstant {
+    int MAX_PAGE_SIZE = 99999;
+
+    String TEMP_CURRENT_USER = "currentUser";
+}
