bf9de88b9a42b0492e1464ae19ba3eab7f95d37e
huhuhan <869108683@qq.com>
2021-10-11 15:38:50 +0800

    SuperEntity时间格式化

--- yh-common/yh-common-db/src/main/java/com/yh/common/db/model/entity/SuperEntity.java
+++ yh-common/yh-common-db/src/main/java/com/yh/common/db/model/entity/SuperEntity.java
@@ -5,8 +5,10 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -22,9 +24,15 @@ public class SuperEntity<T extends Model<?>> extends Model<T> {
 
     @TableId(value = "id_", type = IdType.ASSIGN_ID)
     private Long id;
+
     @TableField(value = "create_time_", fill = FieldFill.INSERT)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+08")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
+
     @TableField(value = "update_time_", fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+08")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     @Override
